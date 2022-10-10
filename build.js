c4fc2b11a2fe9a62d42511f91268384151227735
JDB <legodude17@users.noreply.github.com>
2022-10-10 11:51:21 -0700

    Basic build process

--- build.js
+++ build.js
@@ -1,4 +1,31 @@
 import fs from 'fs/promises';
 import path from 'path';
+import handlebars from 'handlebars';
+import markdownit from 'markdown-it';
+const md = markdownit({
+  html: true,
+  linkify: true,
+  typographer: true
+});
 const distPath = path.resolve('./dist');
-fs.access(distPath).catch(() => fs.mkdir(distPath)).then(() => console.log('Built!'););
+const basePath = path.resolve('./base.html');
+const lessonsPath = path.resolve('./lessons');
+
+async function run() {
+  try {
+    await fs.access(distPath);
+  } catch (e) {
+    await fs.mkdir(distPath);
+  }
+  const base = await fs.readFile(basePath, 'utf-8');
+  await Promise.all((await fs.readdir(lessonsPath)).map(async lesson => {
+    const lessonPath = path.join(lessonsPath, lesson);
+    const resultPath = path.join(distPath, path.basename(lesson) + '.html');
+    const content = md.render(await fs.readFile(lessonPath, 'utf-8'));
+    const template = handlebars.compile(base);
+    const result = template({content});
+    await fs.writeFile(resultPath, result);
+  }))
+}
+
+run();
