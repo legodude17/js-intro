af575f7b8a86bdd048a3704d14834f49be751f56
JDB <legodude17@users.noreply.github.com>
2022-10-11 03:20:02 -0700

    Rewrite Build Script

--- build.js
+++ build.js
@@ -1,69 +1,64 @@
 import fs from 'fs/promises';
 import path from 'path';
 import handlebars from 'handlebars';
-import markdownit from 'markdown-it';
-import blockEmbedPlugin from 'markdown-it-block-embed';
-import VideoServiceBase from 'markdown-it-block-embed/lib/services/VideoServiceBase.js';
-import YAML from 'yaml';
 import resolveCb from "resolve";
-import ll from 'listr-log';
 import {promisify} from 'util';
-import {createStarryNight, common} from '@wooorm/starry-night';
-import {toHtml} from 'hast-util-to-html';
-
-const starryNight = await createStarryNight(common)
+import {unified} from 'unified';
+import remarkParse from 'remark-parse';
+import remarkRehype from 'remark-rehype';
+import rehypeStringify from 'rehype-stringify';
+import remarkGfm from 'remark-gfm';
+import remarkFrontmatter from 'remark-frontmatter';
+import remarkToc from 'remark-toc';
+import rehypeSlug from 'rehype-slug';
+import rehypeAutolink from 'rehype-autolink-headings';
+import rehypeStarryNight from './rehype-starry-night.js';
+import remarkYamlEmit from "./remark-yaml-emit.js";
+import EventEmitter from "events";
+import remarkEmbedder from '@remark-embedder/core';
+import oembedTransformer from '@remark-embedder/transformer-oembed';
+import { read } from 'to-vfile';
+import {h, s} from 'hastscript';
 
 const resolve = promisify(resolveCb);
-const md = markdownit({
-  html: true,
-  linkify: true,
-  typographer: true,
-  highlight(value, lang) {
-    const scope = starryNight.flagToScope(lang)
 
-    return toHtml({
-      type: 'element',
-      tagName: 'pre',
-      properties: {
-        className: scope
-          ? [
-              'highlight',
-              'highlight-' + scope.replace(/^source\./, '').replace(/\./g, '-')
-            ]
-          : undefined
-      },
-      children: scope
-        ? starryNight.highlight(value, scope).children
-        : [{type: 'text', value}]
-    })
-  }
-});
-class ReplItService extends VideoServiceBase {
-  getDefaultOptions() {
-    return { width: '100%', height: 500 };
-  }
+const ee = new EventEmitter();
 
-  extractVideoID(reference) {
-    let match = reference.match(/^.*replit.com\/(@.+)\/([^#]*)#?.*$/);
-    return `${match[1]}/${match[2]}`;
-  }
+const md = unified()
+  .use(remarkParse)
+  .use(remarkGfm)
+  .use(remarkFrontmatter, 'yaml')
+  .use(remarkYamlEmit, ee)
+  .use(remarkToc)
+  .use(remarkEmbedder, {
+    transformers: [oembedTransformer]
+  })
+  .use(remarkRehype)
+  .use(rehypeSlug)
+  .use(rehypeAutolink, {
+    content(node) {
+      return h('span.icon.icon-link.anchor', {ariaHidden: 'true'}, [
+        h('svg.octicon.octicon-link', {
+          ariaHidden: 'true',
+          xmlns: 'http://www.w3.org/2000/svg',
+          viewbox: '0 0 16 16',
+          width: 16,
+          height: 16
+        }, [
+          s('path', {fillRule: 'evenodd', d: "M7.775 3.275a.75.75 0 001.06 1.06l1.25-1.25a2 2 0 112.83 2.83l-2.5 2.5a2 2 0 01-2.83 0 .75.75 0 00-1.06 1.06 3.5 3.5 0 004.95 0l2.5-2.5a3.5 3.5 0 00-4.95-4.95l-1.25 1.25zm-4.69 9.64a2 2 0 010-2.83l2.5-2.5a2 2 0 012.83 0 .75.75 0 001.06-1.06 3.5 3.5 0 00-4.95 0l-2.5 2.5a3.5 3.5 0 004.95 4.95l1.25-1.25a.75.75 0 00-1.06-1.06l-1.25 1.25a2 2 0 01-2.83 0z"})
+        ])
+      ]);
+    }
+  })
+  .use(rehypeStarryNight)
+  .use(rehypeStringify)
+  .freeze();
 
-  getVideoUrl(videoID) {
-    return `//replit.com/${videoID}?embed=true`;
-  }
-}
-md.use(blockEmbedPlugin, {
-  allowFullScreen: false,
-  services: {
-    replit: ReplItService
-  }
-});
 const distPath = path.resolve('./dist');
 const lessonBasePath = path.resolve('./site/lessonBase.html');
 const lessonsPath = path.resolve('./lessons');
 const indexPathIn = path.resolve('./site/index.html');
 const indexPathOut = path.join(distPath, 'index.html');
-const configRE = /^---\n([\s\S]*)\n---/;
 async function copy() {
   return [
     path.resolve('./site/main.css'),
@@ -72,22 +67,18 @@ async function copy() {
 }
 
 async function run() {
-  ll.lessons = 'Lessons';
-  ll.start();
   const lessonTemplate = handlebars.compile(await fs.readFile(lessonBasePath, 'utf-8'), {noEscape: true});
+  const additionalData = {};
+  ee.on('data', (file, data) => additionalData[file] = data);
   const lessons = await Promise.all((await fs.readdir(lessonsPath)).map(async lesson => {
+    const data = {};
     const lessonPath = path.join(lessonsPath, lesson);
     const lessonName = path.basename(lesson, '.md');
-    ll.lessons.addTask({name: lessonName, title: `Lesson ${lessonName}`});
-    const resultFileName = lessonName + '.html';
-    const resultPath = path.join(distPath, resultFileName);
-    const markdown = await fs.readFile(lessonPath, 'utf-8');
-    const match = markdown.match(configRE);
-    const data = match ? YAML.parse(match[1]) : {};
-    data.content = md.render(markdown.replace(configRE, ''));
-    data.path = resultPath;
-    data.url = resultFileName;
+    data.file = lesson;
     data.index = parseInt(lessonName, 10);
+    data.url = lessonName + '.html';
+    data.path = path.join(distPath, data.url);
+    data.content = (await md.process(await read(lessonPath))).toString();
     return data;
   }));
   lessons.sort((a, b) => a.index - b.index);
@@ -95,26 +86,22 @@ async function run() {
     const lesson = lessons[i];
     lesson.prev = lessons[i - 1];
     lesson.next = lessons[i + 1];
+    lessons[i] = {...lesson, ...additionalData[lesson.file]};
   }
   await Promise.all(lessons.map(async lesson => {
     await fs.writeFile(lesson.path, lessonTemplate(lesson));
-    ll.lessons[lesson.index].complete(`${lesson.index + '.md'} -> dist/${lesson.index + '.html'}`);
+    console.error(`lessons/${lesson.index + '.md'} -> dist/${lesson.index + '.html'}`);
   }));
-  ll.index = 'index.html';
   const indexTemplate = handlebars.compile(await fs.readFile(indexPathIn, 'utf-8'));
   await fs.writeFile(indexPathOut, indexTemplate({lessons}));
-  ll.index.complete('Wrote dist/index.html');
-  ll.copy = 'Copy Files';
+  console.error('site/index.html -> dist/index.html');
   await Promise.all((await copy())
     .map(filePath => {
       const fileName = path.basename(filePath);
-      ll.copy.addTask({name: filePath, title: fileName});
       fs.readFile(filePath)
         .then(contents => fs.writeFile(path.join(distPath, fileName), contents))
-        .then(() => ll.copy[filePath].complete(`Wrote dist/${fileName}`));
+        .then(() => console.error(`${path.basename(path.dirname(filePath))}/${fileName} -> dist/${fileName}`));
     }));
-  await new Promise((res) => setTimeout(res, 100));
-  ll.end();
 }
 
 run();
---
+++ remark-yaml-emit.js
@@ -0,0 +1,20 @@
+/**
+ * @typedef {import('mdast').Root} Root
+ * @typedef {import('mdast').YAML} YAML
+ * @typedef {import('mdast-util-to-markdown').Options} Extension
+ */
+
+ import YAML from 'yaml';
+
+ /**
+  * @type {import('unified').Plugin<import('events')[], Root>}
+  */
+ export default function remarkYamlEmit(ee) {
+   return (tree, file) => {
+    if (tree.type !== 'root') return;
+    const node = tree.children[0];
+    if (node.type !== 'yaml') return;
+    const data = YAML.parse(node.value);
+    ee.emit('data', file.basename, data);
+   };
+ }
